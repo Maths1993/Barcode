c96e7929b51a4a8999014c99417e55231ce83e2f
Dominique <dominique.laurencelle@hotmail.de>
2016-05-13 14:33:57 +0200

    Untested wearable code

--- wear/src/main/java/com/example/dominique/barcode/WearMainActivity.java
+++ wear/src/main/java/com/example/dominique/barcode/WearMainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dominique.barcode;
 
+import android.app.ActivityManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,6 +10,7 @@ import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Context;
@@ -16,6 +18,7 @@ import android.support.v4.content.ContextCompat;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class WearMainActivity extends WearableActivity implements SensorEventListener {
@@ -24,12 +27,11 @@ public class WearMainActivity extends WearableActivity implements SensorEventLis
             new SimpleDateFormat("HH:mm", Locale.US);
     private static final String TAG = "TAG";
 
-    private BoxInsetLayout mContainerView;
-    private TextView mTextView;
-    private TextView mClockView;
-
     private SensorManager sensorManager;
     private Sensor accelerometer;
+
+    private Button button;
+    private boolean active = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,34 +39,14 @@ public class WearMainActivity extends WearableActivity implements SensorEventLis
         setContentView(R.layout.activity_phone_main);
         setAmbientEnabled();
 
-        mContainerView = (BoxInsetLayout) findViewById(R.id.container);
-        mTextView = (TextView) findViewById(R.id.text);
-        //mClockView = (TextView) findViewById(R.id.clock);
-
-        // TODO: Test + permission check in manifest (could cause null return)
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if(sensorManager == null) {
-            Toast.makeText(getApplicationContext(),"Couldn't acquire sensor manager",Toast.LENGTH_LONG).show();
-            finish();
-        }
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if(accelerometer == null)  {
-            Toast.makeText(getApplicationContext(),"Accelerometer doesn't exist",Toast.LENGTH_LONG).show();
-            finish();
-        }
-        // TODO: Handle listener when onPause/onEnterAmbient is called
-        boolean result = sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        if(!result) {
-            Toast.makeText(getApplicationContext(),"Couldn't register listener for accelerometer",Toast.LENGTH_LONG).show();
-            finish();
-        }
+        button = (Button) findViewById(R.id.button_gesture);
     }
 
     @Override
     public void onEnterAmbient(Bundle ambientDetails) {
         super.onEnterAmbient(ambientDetails);
-        sensorManager.unregisterListener(this, accelerometer);
         updateDisplay();
+        Log.w(TAG, "enterAmb");
     }
 
     @Override
@@ -76,21 +58,11 @@ public class WearMainActivity extends WearableActivity implements SensorEventLis
     @Override
     public void onExitAmbient() {
         updateDisplay();
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         super.onExitAmbient();
     }
 
     private void updateDisplay() {
-        if (isAmbient()) {
-            mContainerView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), android.R.color.black));
-            mTextView.setTextColor(ContextCompat.getColor(getApplicationContext(), android.R.color.white));
-            //mClockView.setVisibility(View.VISIBLE);
-            //mClockView.setText(AMBIENT_DATE_FORMAT.format(new Date()));
-        } else {
-            mContainerView.setBackground(null);
-            mTextView.setTextColor(ContextCompat.getColor(getApplicationContext(), android.R.color.black));
-            //mClockView.setVisibility(View.GONE);
-        }
+
     }
 
     @Override
@@ -108,5 +80,43 @@ public class WearMainActivity extends WearableActivity implements SensorEventLis
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    public void handleAccelerometer(View view) {
+        if(!active) {
+            sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+            if (sensorManager == null) {
+                Toast.makeText(getApplicationContext(), "Couldn't acquire sensor manager", Toast.LENGTH_LONG).show();
+                return;
+            }
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            if (accelerometer == null) {
+                Toast.makeText(getApplicationContext(), "Accelerometer doesn't exist", Toast.LENGTH_LONG).show();
+                return;
+            }
+            boolean result = sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            if (!result) {
+                Toast.makeText(getApplicationContext(), "Couldn't register listener for accelerometer", Toast.LENGTH_LONG).show();
+                return;
+            }
+            button.setText("END GESTURE RECOGNITION");
+            active = true;
+        } else {
+            sensorManager.unregisterListener(this, accelerometer);
+            button.setText("START GESTURE RECOGNITION");
+            active = false;
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.w(TAG, "pause");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.w(TAG, "resume");
     }
 }
