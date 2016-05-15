012e159d256ea4b8fe472ca5566ba758b332a14f
Dominique <dominique.laurencelle@hotmail.de>
2016-05-15 18:23:26 +0200

    Implemented file for recording "ideal gestures" which will be used for recognizing gesture for starting scanning

---
+++ mobile/src/main/java/com/example/dominique/barcode/WatchToGlass.java
@@ -0,0 +1,14 @@
+package com.example.dominique.barcode;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WatchToGlass extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
