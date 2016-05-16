d7913ab724e15b981933e4323c409e651fdfeeb8
Schonhart <s.schonhart@t-online.de>
2016-05-16 13:00:48 +0200

    changesa at watchtoglass

--- mobile/src/main/java/com/example/dominique/barcode/WatchToGlass.java
+++ mobile/src/main/java/com/example/dominique/barcode/WatchToGlass.java
@@ -1,63 +1,85 @@
 package com.example.dominique.barcode;
-import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
-import android.util.Log;
-
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.ConnectionResult;
+import
+        com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.android.gms.wearable.Asset;
+import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.DataEvent;
 import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public class WatchToGlass extends Activity {
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
+public class WatchToGlass extends Activity implements
+        DataApi.DataListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+    private TextView mTextView;
+    private static final long CONNECTION_TIME_OUT_MS = 100;
+    private static final String ON_MESSAGE = "On!";
+    private static final String OFF_MESSAGE = "Off!";
+    private static final String TAG = "MobistelDisplayControl";
+
+    private String nodeId;
+
     private GoogleApiClient mGoogleApiClient;
+
+
     private  void startApiClient() {
-        try
+    mGoogleApiClient = getGoogleApiClient(this);
+    retrieveDeviceNode();
+    }
 
-        {
-            Log.d("dsf", "enter try " );
-            GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(new ConnectionCallbacks() {
-                        @Override
-                        public void onConnected(Bundle connectionHint) {
-                            Log.d("dsf", "onConnected: " + connectionHint);
+    private void retrieveDeviceNode() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mGoogleApiClient.blockingConnect(CONNECTION_TIME_OUT_MS, TimeUnit.MILLISECONDS);
+                NodeApi.GetConnectedNodesResult result =
+                        Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).await();
+                List<Node> nodes = result.getNodes();
+                if (nodes.size() > 0) {
+                    nodeId = nodes.get(0).getId();
+                }
+                mGoogleApiClient.disconnect();
+            }
+        }).start();
+    }
 
-                        }
-
-                        @Override
-                        public void onConnectionSuspended(int cause) {
-                            Log.d("suspended", "onConnectionSuspended: " + cause);
-                        }
-                    })
-                    .addOnConnectionFailedListener(new OnConnectionFailedListener() {
-                        @Override
-                        public void onConnectionFailed(ConnectionResult result) {
-                            Log.d("failed", "onConnectionFailed: " + result);
-                        }
-                    })
-
-                    .addApi(Wearable.API)
-                    .build();
-        } catch (Exception e)
-
-        {
-           e.printStackTrace();
-        }
+    private GoogleApiClient getGoogleApiClient(Context context) {
+        return new GoogleApiClient.Builder(context)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,17 +89,11 @@ public class WatchToGlass extends Activity {
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-    }
-
-
-    public void sentToGlass() {
-        putInDataStore();
-    }
-
-    public void receiveFromGlass() {
 
     }
+
+
+
 
     private Asset createAnAsset(int message) {
 
@@ -90,65 +106,79 @@ public class WatchToGlass extends Activity {
         Asset asset = createAnAsset(1);
         PutDataRequest request = PutDataRequest.create("/glassconnection");
         request.putAsset("connection", asset);
-        Wearable.DataApi.putDataItem(client, request);
+        Wearable.DataApi.putDataItem(mGoogleApiClient, request);
     }
 
 
+
+    public void onStart() {
+        super.onStart();
+
+
+
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
     public void onDataChanged(DataEventBuffer dataEvents) {
-
+        Toast.makeText(this, "DataChanged!", Toast.LENGTH_LONG).show();
         for (DataEvent event : dataEvents) {
-            if (event.getType() == DataEvent.TYPE_CHANGED &&
-                    event.getDataItem().getUri().getPath().equals("/glassconnection")) {
+            if (event.getType() == DataEvent.TYPE_CHANGED && event.getDataItem().getUri().getPath().equals("/image")) {
                 DataMapItem dataMapItem = DataMapItem.fromDataItem(event.getDataItem());
-                Asset profileAsset = dataMapItem.getDataMap().getAsset("connection");
-
-
+                Asset profileAsset = dataMapItem.getDataMap().getAsset("profileImage");
+                Bitmap bitmap = loadBitmapFromAsset(profileAsset);
+                // Do something with bitmap
+                Toast.makeText(this, "DataChanged!", Toast.LENGTH_LONG).show();
             }
         }
     }
 
-    public void onStart() {
-        super.onStart();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-    mGoogleApiClient.connect();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "WatchtoGlass Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.dominique.barcode/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
+    public Bitmap loadBitmapFromAsset(Asset asset) {
+        if (asset == null) {
+            throw new IllegalArgumentException("Asset must be non-null");
+        }
 
-    public void onStop() {
-        super.onStop();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "WatchtoGlass Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.dominique.barcode/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
+        ConnectionResult result = mGoogleApiClient.blockingConnect(CONNECTION_TIME_OUT_MS, TimeUnit.MILLISECONDS);
+        if (!result.isSuccess()) {
+            return null;
+        }
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.disconnect();
+        // Convert asset into a file descriptor and block until it's ready
+        InputStream assetInputStream = Wearable.DataApi.getFdForAsset(mGoogleApiClient, asset).await().getInputStream();
         mGoogleApiClient.disconnect();
+
+        if (assetInputStream == null) {
+            Log.w(TAG, "Requested an unknown Asset.");
+            return null;
+        }
+
+        // Decode the stream into a bitmap
+        return BitmapFactory.decodeStream(assetInputStream);
     }
 
 
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Wearable.DataApi.addListener(mGoogleApiClient, this);
+        Toast.makeText(this, "AddedListener!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this, "ConnectionSuspended", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "ConnectionFailed", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        Wearable.DataApi.removeListener(mGoogleApiClient, this);
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
 }
