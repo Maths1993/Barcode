6e6ccf32cf300259ef1e33731a867f4f6d9aa0c2
Schonhart <s.schonhart@t-online.de>
2016-05-16 09:37:30 +0200

    watch  to glass

--- mobile/src/main/java/com/example/dominique/barcode/WatchToGlass.java
+++ mobile/src/main/java/com/example/dominique/barcode/WatchToGlass.java
@@ -1,14 +1,165 @@
 package com.example.dominique.barcode;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import
+        com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class WatchToGlass extends Activity {
+import com.google.android.gms.wearable.Asset;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 
+public class WatchtoGlass extends Activity {
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private GoogleApiClient mGoogleApiClient;
+    private  void startApiClient() {
+        try
+
+        {
+            Log.d("dsf", "enter try " );
+            GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(new ConnectionCallbacks() {
+                        @Override
+                        public void onConnected(Bundle connectionHint) {
+                            Log.d("dsf", "onConnected: " + connectionHint);
+
+                        }
+
+                        @Override
+                        public void onConnectionSuspended(int cause) {
+                            Log.d("suspended", "onConnectionSuspended: " + cause);
+                        }
+                    })
+                    .addOnConnectionFailedListener(new OnConnectionFailedListener() {
+                        @Override
+                        public void onConnectionFailed(ConnectionResult result) {
+                            Log.d("failed", "onConnectionFailed: " + result);
+                        }
+                    })
+
+                    .addApi(Wearable.API)
+                    .build();
+        } catch (Exception e)
+
+        {
+           e.printStackTrace();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
+    startApiClient();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+
+    public void sentToGlass() {
+        putInDataStore();
+    }
+
+    public void receiveFromGlass() {
+
+    }
+
+    private Asset createAnAsset(int message) {
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(message);
+        return Asset.createFromBytes(outputStream.toByteArray());
+
+    }
+
+    private void putInDataStore() {
+        Asset asset = createAnAsset(1);
+        PutDataRequest request = PutDataRequest.create("/glassconnection");
+        request.putAsset("connection", asset);
+        Wearable.DataApi.putDataItem(client, request);
+    }
+
+
+    public void onDataChanged(DataEventBuffer dataEvents) {
+
+        for (DataEvent event : dataEvents) {
+            if (event.getType() == DataEvent.TYPE_CHANGED &&
+                    event.getDataItem().getUri().getPath().equals("/glassconnection")) {
+                DataMapItem dataMapItem = DataMapItem.fromDataItem(event.getDataItem());
+                Asset profileAsset = dataMapItem.getDataMap().getAsset("connection");
+
+
+            }
+        }
+    }
+
+    public void onStart() {
+        super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+    mGoogleApiClient.connect();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "WatchtoGlass Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.dominique.barcode/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    public void onStop() {
+        super.onStop();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "WatchtoGlass Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.dominique.barcode/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.disconnect();
+        mGoogleApiClient.disconnect();
+    }
+
+
 }
