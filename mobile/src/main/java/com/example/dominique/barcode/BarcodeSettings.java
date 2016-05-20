2688800fdbeab43fbfc31b4073b71d04cb819d3a
Dominique <dominique.laurencelle@hotmail.de>
2016-05-20 19:50:20 +0200

    New code for storing barcode in online database

--- mobile/src/main/java/com/example/dominique/barcode/BarcodeSettings.java
+++ mobile/src/main/java/com/example/dominique/barcode/BarcodeSettings.java
@@ -3,56 +3,52 @@ package com.example.dominique.barcode;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class BarcodeSettings extends Activity implements View.OnClickListener {
 
     private static final String responseName = "0";
 
-    private Button button_apply;
+    private EditText text_barcode_edit;
     private EditText text_description_edit;
     private EditText text_price_edit;
     private EditText text_location_edit;
-    private TextView text_description;
-    private TextView text_price;
-    private TextView text_location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.barcode_settings);
-
-        button_apply = (Button) findViewById(R.id.button_apply);
         init();
     }
 
     public void init() {
-
+        text_barcode_edit = (EditText) findViewById(R.id.text_barcode_edit);
         text_description_edit = (EditText) findViewById(R.id.text_description_edit);
         text_price_edit = (EditText) findViewById(R.id.text_price_edit);
         text_location_edit = (EditText) findViewById(R.id.text_location_edit);
-        button_apply = (Button) findViewById(R.id.button_apply);
+
         // Set default values for already existing barcode
         if((getIntent().hasExtra("store") && getIntent().getStringExtra("store").equals("duplicate"))
-                || getIntent().hasExtra("search")) {
-                text_description_edit.setText(getIntent().getStringExtra("description"));
-                text_price_edit.setText(getIntent().getStringExtra("price"));
-                text_location_edit.setText(getIntent().getStringExtra("location"));
+                || getIntent().hasExtra("itemClick")) {
+            Log.w("task1", "here i am");
+            Log.w("task2", getIntent().getStringExtra("code"));
+            text_barcode_edit.setText(getIntent().getStringExtra("code"));
+            text_description_edit.setText(getIntent().getStringExtra("description"));
+            text_price_edit.setText(getIntent().getStringExtra("price"));
+            text_location_edit.setText(getIntent().getStringExtra("location"));
         }
     }
 
     @Override
     public void onClick(View v) {
         Intent returnIntent = new Intent();
+        returnIntent.putExtra("code", text_barcode_edit.getText().toString());
         returnIntent.putExtra("description", text_description_edit.getText().toString());
         returnIntent.putExtra("price", text_price_edit.getText().toString());
         returnIntent.putExtra("location", text_location_edit.getText().toString());
         setResult(PhoneToDatabase.OK, returnIntent);
-        Toast.makeText(getApplicationContext(), text_description_edit.getText(), Toast.LENGTH_LONG).show();
         finish();
     }
 }
--- mobile/src/main/java/com/example/dominique/barcode/PhoneToDatabase.java
+++ mobile/src/main/java/com/example/dominique/barcode/PhoneToDatabase.java
@@ -3,16 +3,24 @@ package com.example.dominique.barcode;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PhoneToDatabase extends Activity {
@@ -20,41 +28,137 @@ public class PhoneToDatabase extends Activity {
     private static final String responseName = "1";
     private static final int requestCode = 1;
     public static final int OK = 1;
-    public static final int NOT_OK = 2;
+    public static final int DELETE = 2;
 
     private Firebase database;
     private String barcode;
-    private String description;
-    private String price;
-    private String location;
 
     private TextView text_barcode;
+    private ListView listView;
+    private SimpleAdapter adapter;
 
-    public static String[] map = { "1", "2",
-            "3", "4" };
+    private final String[] from = {"code", "description", "price", "location"};
+    private final int[] to = {R.id.code, R.id.description, R.id.price, R.id.location};
+    private List<Map<String, ?>> data = new ArrayList<Map<String, ?>>();
+    private Map<String, Map> cache = new HashMap<String, Map>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.phone_to_database);
-/*
-        ArrayAdapter<String> itemsAdapter =
-                new ArrayAdapter<String>(this, R.layout.row_layout, map);
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(itemsAdapter);*/
 
         if(getIntent().hasExtra("barcode")) {
             // Get barcode
             barcode = getIntent().getStringExtra("barcode");
 
+            // Set up barcode text view
             text_barcode = (TextView) findViewById(R.id.text_barcode);
             text_barcode.setText("Scanned code: " + barcode);
+            listView = (ListView) findViewById(R.id.listView);
 
             // Set up database
             Firebase.setAndroidContext(this);
             database = new Firebase("https://torrid-heat-574.firebaseio.com/");
-        } else finish();
 
+            // Set up list view and adapter to show contents
+            adapter = new SimpleAdapter(this, data, R.layout.row_layout, from, to);
+            listView.setAdapter(adapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String code = (String) ((TextView) view.findViewById(R.id.code)).getText();
+                    String description = (String) ((TextView) view.findViewById(R.id.description)).getText();
+                    String price = (String) ((TextView) view.findViewById(R.id.price)).getText();
+                    String location = (String) ((TextView) view.findViewById(R.id.location)).getText();
+                    Intent requestIntent = new Intent(getApplicationContext(), BarcodeSettings.class);
+                    requestIntent.putExtra("itemClick", "itemClick");
+                    requestIntent.putExtra("code", code);
+                    requestIntent.putExtra("description", description);
+                    requestIntent.putExtra("price", price);
+                    requestIntent.putExtra("location", location);
+                    startActivityForResult(requestIntent, requestCode);
+                }
+
+            });
+
+            listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+                @Override
+                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                    String code = (String) ((TextView) view.findViewById(R.id.code)).getText().toString();
+                    Intent requestIntent = new Intent(getApplicationContext(), AskToDelete.class);
+                    requestIntent.putExtra("delete", "delete");
+                    requestIntent.putExtra("code", code);
+                    startActivityForResult(requestIntent, requestCode);
+                    return true;
+                }
+
+            });
+
+            database.addChildEventListener(new ChildEventListener() {
+
+                String code;
+                String description;
+                String price;
+                String location;
+
+                @Override
+                public void onChildAdded(DataSnapshot dSnap, String s) {
+                    init(dSnap);
+                    // Add new entry
+                    adapter.notifyDataSetChanged();
+                    data.add(createRow(code, description, price, location));
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dSnap, String s) {
+                    init(dSnap);
+                    // Remove old entry
+                    adapter.notifyDataSetChanged();
+                    data.remove(cache.get(code));
+                    // Add new entry
+                    adapter.notifyDataSetChanged();
+                    data.add(createRow(code, description, price, location));
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    // Remove old entry
+                    adapter.notifyDataSetChanged();
+                    data.remove(cache.get(code));
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                    Log.w("MOVED", "moved");
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+                    Log.w("CANCELED", "canceled");
+                }
+
+                public void init(DataSnapshot dSnap) {
+                    code = (String) dSnap.child("code").getValue().toString();
+                    description = (String) dSnap.child("description").getValue().toString();
+                    price = (String) dSnap.child("price").getValue().toString();
+                    location = (String) dSnap.child("location").getValue().toString();
+                }
+
+            });
+        } else finish();
+    }
+
+    private Map<String, ?> createRow(String code, String description, String price, String location) {
+        Map<String, String> row = new HashMap<String, String>();
+        row.put("code", code);
+        row.put("description", description);
+        row.put("price", price);
+        row.put("location", location);
+        cache.put(code, row);
+        return row;
     }
 
     public void storeInDatabase(View view) {
@@ -92,59 +196,26 @@ public class PhoneToDatabase extends Activity {
         });
     }
 
-    public void searchInDatabase(View view) {
-        database.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                // Barcode already in database
-                if(snapshot.hasChild(barcode)) {
-                    Toast.makeText(getApplicationContext(), "Barc" +
-                            "ode " + barcode + " already in database!" +
-                            " You can edit and overwrite now", Toast.LENGTH_LONG).show();
-                    Intent requestIntent = new Intent(getApplicationContext(), BarcodeSettings.class);
-
-                    for(DataSnapshot snap : snapshot.child(barcode).getChildren()) {
-                        requestIntent.putExtra(snap.getKey(), (String) snap.getValue());
-                    }
-
-                    requestIntent.putExtra("search", "duplicate");
-                    startActivityForResult(requestIntent, requestCode);
-                }
-                // Barcode not in database
-                else {
-                    Toast.makeText(getApplicationContext(), "Barcode not found", Toast.LENGTH_LONG).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(FirebaseError error) {
-            }
-        });
-    }
-
-    public void rejectFromDatabase(View view) {
-        Intent returnIntent = new Intent();
-        returnIntent.putExtra(responseName, WatchToGlass.OK);
-        setResult(WatchToGlass.OK, returnIntent);
-        finish();
-    }
-
     @Override
     public void onActivityResult(int receivedCode, int resultCode, Intent data) {
+
         if(receivedCode == requestCode) {
             if(resultCode == OK) {
+                String code = data.getStringExtra("code");
                 String description = data.getStringExtra("description");
                 String price = data.getStringExtra("price");
                 String location = data.getStringExtra("location");
 
-                Barcodes obj = new Barcodes(barcode, description, price, location);
+                Barcodes obj = new Barcodes(code, description, price, location);
                 Map<String, Object> map = new ObjectMapper().convertValue(obj, Map.class);
-                database.child(barcode).updateChildren(map);
+                database.child(code).updateChildren(map);
 
-                // database.setValue();
                 Intent returnIntent = new Intent();
                 returnIntent.putExtra(responseName, WatchToGlass.OK);
                 setResult(WatchToGlass.OK, returnIntent);
+            }
+            if(resultCode == DELETE) {
+                database.child(data.getStringExtra("code")).removeValue();
             }
         }
     }
