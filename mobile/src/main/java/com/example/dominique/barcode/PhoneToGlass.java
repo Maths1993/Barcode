feb6605b7caf69795debebccd3622065596c77ac
Dominique <dominique.laurencelle@hotmail.de>
2016-07-03 23:17:13 +0200

    restored old file

--- mobile/src/main/java/com/example/dominique/barcode/PhoneToGlass.java
+++ mobile/src/main/java/com/example/dominique/barcode/PhoneToGlass.java
@@ -12,6 +12,8 @@ public class PhoneToGlass extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BluetoothHelper.getInstance(getApplicationContext()).connect();
+
        // FirebaseMessaging.getInstance().subscribeToTopic("central");
 
         //new FirebaseServer("AIzaSyCXmt761UPr1z3DvHDY2t9Sfrne4lEnsD4").sendDataToTopic("glass", FirebaseServer.stringToMap("cmd", "SCAN_RESPONSE", "status", "NOK", "value", "Stinkender Dödel"));
