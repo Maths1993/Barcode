2ea3b80f16bebdbdfb5c08144c08ab858845ca76
Paul Gavrikov <bluewhile8@gmail.com>
2016-07-04 16:32:59 +0200

    Added BLE error handling

--- mobile/src/main/java/com/example/dominique/barcode/PhoneToGlass.java
+++ mobile/src/main/java/com/example/dominique/barcode/PhoneToGlass.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.motioncoding.firebaseserver.FirebaseServer;
 
 
 public class PhoneToGlass extends Activity {
@@ -17,7 +18,13 @@ public class PhoneToGlass extends Activity {
         FirebaseMessaging.getInstance().subscribeToTopic("central");
 
         //new FirebaseServer("AIzaSyCXmt761UPr1z3DvHDY2t9Sfrne4lEnsD4").sendDataToTopic("glass", FirebaseServer.stringToMap("cmd", "SCAN_RESPONSE", "status", "NOK", "value", "Stinkender Dödel"));
+        try {
         BluetoothHelper.getInstance(getApplicationContext()).connect();
+    } catch (Exception e) {
+            new FirebaseServer("AIzaSyCXmt761UPr1z3DvHDY2t9Sfrne4lEnsD4")
+                    .sendDataToTopic("glass", FirebaseServer.stringToMap("cmd", "SCAN_RESPONSE",
+                            "status", "ERROR", "value", "Scanner not connected"));
+        }
 
       //  GPSTracker gps = new GPSTracker(PhoneToGlass.this);
 
