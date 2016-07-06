f1b6befa767045d41f89d3d767ed64efd113542a
Paul Gavrikov <bluewhile8@gmail.com>
2016-07-06 18:47:40 +0200

    Glass Response

---
+++ mobile/src/main/java/com/example/dominique/barcode/MyFirebaseMessagingService.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.dominique.barcode;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.e(TAG, "From: " + remoteMessage.getFrom());
+        if(remoteMessage.getData().get("cmd").equals("SCAN")) {
+            BluetoothHelper.getInstance(getApplicationContext()).startBarcodeScan(); //TODO
+        }
+
+
+
+//        Toast.makeText(getApplicationContext(), remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT).show();
+    }
+
+}
